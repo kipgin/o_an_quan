f3f089612f3437664d2afee310dee2a218688988
kipgin <ductamhyntt@gmail.com>
2026-01-08 20:34:58 +0700

    fix maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,8 @@ module OAnQuanMaven {
 
     opens com.code to javafx.graphics, javafx.fxml;
     opens com.code.controller to javafx.fxml;
-    opens css to javafx.graphics, javafx.fxml;
-    opens image to javafx.graphics,javafx.fxml;
-    // opens com.code.view to javafx.fxml;
+    // opens css to javafx.graphics, javafx.fxml;
+    // opens image to javafx.graphics,javafx.fxml;
+    opens com.code.view to javafx.fxml;
     exports com.code;
 }
